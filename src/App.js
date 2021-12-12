3c4003f94a4a2b525a4c1189b914ae16f2101c09
KnechtRootrechtCH <thomas@atheos.cc>
2021-12-12 14:24:20 +0100

    react-document-meta added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
+import DocumentMeta from 'react-document-meta';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const meta = {
+    title: 'Caelus',
+    description: 'SC Hangar Manager',
+    neta: {
+      viewport: 'initial-scale=1, width=device-width'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          !--TEST_SECRET--!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DocumentMeta {...meta}>
+      <div className="App">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </DocumentMeta>
   );
 }
 
